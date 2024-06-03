026948383bfc77839c319f48a7db2cb4ad8e1ff8
Innoisig <bahatiinnocent36@gmail.com>
2024-06-03 03:59:26 -0700

    jeu divinette

---
+++ jeu_divinette.py
@@ -0,0 +1,24 @@
+import random
+
+def jeu_devinette():
+    nombre_a_deviner = random.randint(1, 100)
+    nombre_essai = 0
+
+    print("Bienvenu dans le jeu de devinette")
+    print("Je pense entre 1 et 100. Pouvez vous deviner quel est ce nombre")
+
+    while True:
+        essaie = int(input("Entrez votre divinette"))
+        nombre_essai += 1
+
+        if essaie < nombre_a_deviner:
+            print(f"Le nombre  est trop petit")
+        elif essaie > nombre_a_deviner:
+            print(f"Le nombre  est trop grang")
+        else: 
+            print(f"Felicitation ! Vous avez diviner un nombre en {nombre_a_deviner} assai {'s' if nombre_a_deviner > 1 else ''}")
+            break
+
+
+if __name__ == "__main__":
+    jeu_devinette()
