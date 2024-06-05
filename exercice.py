a77915e13019eec42cbf42f4b4d0b138872a0c4d
Innoisig <bahatiinnocent36@gmail.com>
2024-06-05 06:25:45 -0700

    ajout

--- exercice.py
+++ exercice.py
@@ -15,3 +15,22 @@ def add(a: int, b: int) -> int:
     return a + b
 
 add(1, 3)
+
+#affichage fonction multiple
+def list(*list_item):
+    for item in list_item:
+        print(item)
+list("epee")
+list("Soupe", "Pima", "tomate", "Gout")
+
+#Difference entre le plus grand nombre et le plus petit
+
+def la_difference(nombre1, nombre2):
+    if nombre1 > nombre2:
+        return nombre1
+    elif nombre1 < nombre2:
+        return nombre2
+    else:
+        return "C'est l'égalité"
+    
+print(la_difference(4, 4))
